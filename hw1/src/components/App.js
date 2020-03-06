0e88b6ca5f142db930bae113a35161f114835b2a
grey4uk <grey4uk14@gmail.com>
2020-03-07 00:52:00 +0200

    all tasks of hw1 added

--- hw1/src/components/App.js
+++ hw1/src/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import profile from './profile.json.js';
-import Card from './card';
+import React from "react";
+import profile from "./profile.json";
+import Card from "./card";
+import StatisticalData from "./statisticalData";
+import statistic from "./statistic.json";
+import friends from './friends.json';
+import FriendsList from './friendsData';
+import transactions from "./transactions.json";
+import Transactions from "./transactionsData"
 
-const App = () => <Card key={profile.name} {...profile}/>;
+
+const App = () => {
+  return (
+    <>
+      <Card key={profile.name} {...profile} />
+      <StatisticalData title="Upload stats" stats={statistic} />
+      <FriendsList props={friends}/>
+      <Transactions items={transactions} />
+    </>
+  );
+};
 
 export default App;
